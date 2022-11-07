c8487450f7424cb25f79e4712f9ba13af2016551
beki-d <bk14drobs@gmail.com>
2022-11-07 12:57:35 +0300

    cors

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const { default: mongoose } = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 require('dotenv/config');
 
+//Middlewares
 app.use(bodyParser.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
